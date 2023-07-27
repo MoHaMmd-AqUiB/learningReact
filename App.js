5ba1c421f3c40da173ee1a3b54b76ca4731aa779
Mohammad Aquib <mohdaquib154@gmail.com>
2023-07-27 17:53:22 +0530

    lecture 4 completed

--- App.js
+++ App.js
@@ -1,3 +1,9 @@
-const heading=React.createElement("h1",{},"Hello");
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import React from  "react";
+import ReactDOM from "react-dom/client";
+
+const heading=React.createElement("h1",{},"Hello Janeman");
+const heading2=<h2> Hello !</h2>
+console.log(heading);
+console.log(heading2)
+const root=ReactDOM.createRoot(document.getElementById("aquib"));
+root.render(heading2);
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {}, "Hello Janeman");
+const root = ReactDOM.createRoot(document.getElementById("aquib"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
